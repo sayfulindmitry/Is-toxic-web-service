737f5b9681bc64e2dd9c727b90d9322f3ba4f6e8
Dmitry Sayfulin <54313653+sayfulindmitry@users.noreply.github.com>
2019-08-29 16:14:26 +0300

    Update log_reg_model.py

--- log_reg_model.py
+++ log_reg_model.py
@@ -1,10 +1,7 @@
-#Импортируем все нужные библиотеки
+# Импортируем все нужные библиотеки
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
+import pandas as pd 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -18,8 +15,7 @@ from sklearn.metrics import roc_curve, auc, roc_auc_score
 from sklearn.externals import joblib
 
 
-#Сохраняем данные, заполняем пропущенные значения
-
+# Сохраняем данные, заполняем пропущенные значения
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
@@ -27,37 +23,33 @@ df = train_df.copy()
 df.dropna(axis=1, inplace=True)
 
 
-#Векторизуем комментарии, поскольку наша модель будет работать с числами.
-#Удобной статистикой для этого является Tf-idf
-
+# Векторизуем комментарии, поскольку наша модель будет работать с числами.
+# Удобной статистикой для этого является Tf-idf
 Vectorize = TfidfVectorizer()
 
 X = Vectorize.fit_transform(df["comment_text"])
 
 test_X = Vectorize.transform(test_df["comment_text"])
 
-#Объявим целевую переменную через следующий критерий:
-
+# Объявим целевую переменную через следующий критерий:
 y = np.where(train_df['target'] >= 0.5, 1, 0)
 
-#Для обучения модели нам нужно разделить датасет (2/3 и 1/3)
-
+# Для обучения модели нам нужно разделить датасет (2/3 и 1/3)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=42)
 
-#Теперь мы готовы для создания и обучения модели (будем использовать логистическую регрессию)
+# Теперь мы готовы для создания и обучения модели (будем использовать логистическую регрессию)
 lr = LogisticRegression(C=5, random_state=42, solver='sag', max_iter=1000, n_jobs=-1)
 lr.fit(X_train, y_train)
 
-#Проверим score на кросс-валидации
+# Проверим score на кросс-валидации
 cv_accuracy = cross_val_score(lr, X, y, cv=5, scoring='roc_auc')
 
-print(cv_accuracy) #[0.94607755 0.94436349 0.94531875 0.94375522 0.94326619]
-print(cv_accuracy.mean()) #0.9445562409622392
+print(cv_accuracy) # [0.94607755 0.94436349 0.94531875 0.94375522 0.94326619]
+print(cv_accuracy.mean()) # 0.9445562409622392
 
-#Модель обучилась отлично!
+# Модель обучилась отлично! На kaggle auc_score ~ 0.9
 
-#Осталось сохранить векторизация и саму модель для внедрения ее в веб-сервис
+# Осталось сохранить векторизация и саму модель для внедрения ее в веб-сервис
 
 joblib.dump(lr, 'log_reg_model.pkl')
 joblib.dump(Vectorize, 'vectorize.pkl')
-
